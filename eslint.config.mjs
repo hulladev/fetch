0ff9228afc4db36edcd966beb585cf716aae0fd2
samuelhulla <oficialny.hulla@gmail.com>
2024-07-01 16:16:43 +0200

    :screwdriver: config: (repo) eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,23 @@
+// @ts-check
+import eslint from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import prettier from 'eslint-plugin-prettier/recommended'
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  prettier,
+  {
+    rules: {
+      '@typescript-eslint/ban-types': [
+        "error",
+        {
+          types: {
+            '{}': false
+          }
+        }
+      ],
+      '@typescript-eslint/no-explicit-any': 'off',
+    }
+  }
+)
